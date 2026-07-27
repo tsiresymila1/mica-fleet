78c768ee4b0144fcbcd558b0c36184b30ecf5dc2
Tsiresy Milà <tsiresymila@gmail.com>
2026-07-27 11:33:45 +0300

    chore: configure Android packaging options for dependency conflicts and add VS Code Java configuration settings

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -66,6 +66,25 @@ android {
             )
         }
     }
+     packaging {
+        jniLibs {
+            useLegacyPackaging = true
+        }
+        resources {
+            excludes += setOf(
+                "META-INF/DEPENDENCIES",
+                "META-INF/LICENSE",
+                "META-INF/LICENSE.txt",
+                "META-INF/license.txt",
+                "META-INF/NOTICE",
+                "META-INF/NOTICE.txt",
+                "META-INF/notice.txt",
+                "META-INF/ASL2.0",
+                "META-INF/*.kotlin_module",
+                "META-INF/versions/**"
+            )
+        }
+    }
 }
 
 flutter {
